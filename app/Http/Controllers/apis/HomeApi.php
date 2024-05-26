1bc4ef719a66ba8c3655cfec8b4f0f06c66fa854
elsadany <eng.ahmedelsadany1@gmail.com>
2024-05-26 14:54:07 +0300

    commit

--- app/Http/Controllers/apis/HomeApi.php
+++ app/Http/Controllers/apis/HomeApi.php
@@ -128,8 +128,8 @@ class HomeApi extends Controller
         $user_ids = array_merge($user_ids, UserFriend::where('friend_id', $request->user()->id)->pluck('user_id')->toArray());
         $user_blocks=UserBlock::where('user_id',$request->user()->id)->pluck('friend_id')->toArray();
         $user_ids=array_merge([$request->user()->id],$user_ids);
-        $stories = User::latest('id')->whereNotIn('id',$user_ids)->with('stories')->whereHas('stories')->paginate(12);
-        $posts = User::latest('id')->whereNotIn('id',$user_ids)->whereIn("id", $user_ids)->with('stories')->whereHas('stories')->paginate(12);
+        $stories = User::latest('id')->whereNotIn('id',$user_blocks)->with('stories')->whereHas('stories')->paginate(12);
+        $posts = User::latest('id')->whereNotIn('id',$user_blocks)->whereIn("id", $user_ids)->with('stories')->whereHas('stories')->paginate(12);
         return response()->json([
             'status' => true,
             'code' => 200,
