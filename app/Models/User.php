c0b2875315ebf8ce0d33088c943a2aace97eff20
elsadany <eng.ahmedelsadany1@gmail.com>
2024-06-26 19:59:31 +0300

    commti

--- app/Models/User.php
+++ app/Models/User.php
@@ -100,7 +100,7 @@ class User extends Authenticatable
 
     function getUnreadTicketsAttribute()
     {
-        return $this->tickets()->where('is_read', 0)->count();
+        return $this->notifications()->where('is_read', 0)->count();
     }
 
     function getFollowersAttribute()
