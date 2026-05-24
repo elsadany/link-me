f1a5d21315def8d92242eeeb8272dd2504039d9f
ahmed.elsadany <eng.ahmedelsadany1@gmail.com>
2026-05-24 23:56:03 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Hello World'
+    ]);
 });
 
 Route::get('/test', [\App\Http\Controllers\TestController::class, 'uploadToSpace']);
